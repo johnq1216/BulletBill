a5e5e481fdd4dc5c37319d71ffcd67c0cc70cdec
John <johnq1216@gmail.com>
2014-06-08 01:35:37 -0700

    Firstr Commit

---
+++ Assets/Scripts/Dpad.js
@@ -0,0 +1,85 @@
+﻿
+private var fp : Vector2;  // first finger position
+private var lp : Vector2;  // last finger position
+
+var cooldown : int;
+
+var dpadSecondCooldown : int = 30;
+
+
+
+var touchZone : Rect;
+var BounceAmount : float = 14;
+var Dive : float = 20;
+var dpadLeft : GUITexture;
+var dpadRight : GUITexture;
+var dpadAction : GUITexture;
+var dpadSecond : GUITexture;
+
+var player : Transform;
+
+
+
+function Start (){
+	cooldown = dpadSecondCooldown;
+}
+
+
+function Update () {
+		
+	cooldown--;
+	
+
+
+	for (var touch : Touch in Input.touches){
+		if (touch.phase == TouchPhase.Began){
+			fp = touch.position;
+			lp = touch.position;
+		}
+		
+		if (touch.phase == TouchPhase.Moved ){
+			lp = touch.position;
+		}
+		
+//		if(cooldown <= 0){
+		if(true){
+
+ 			if(dpadLeft.HitTest(touch.position)){
+ 		 
+					player.GetComponent(Movement).MoveUp();
+					Debug.Log ("HitTest: The Touch is down on LEFT");
+		
+			}else if(dpadRight.HitTest(touch.position)){
+			
+ 		            player.GetComponent(Movement).MoveDown();
+					Debug.Log ("HitTest: The Touch is down on RIGHT");
+				
+			}else if(dpadAction.HitTest(touch.position)){
+ 		 
+//					player.GetComponent(FPSInputController).dpadSwiped = true;
+					Debug.Log ("HitTest: The Touch is down on " + this.name);
+				
+			}else if(dpadSecond.HitTest(touch.position) && cooldown <=0){
+
+						
+//					   	 player.GetComponent(DoubleJump).Dive();
+//					  	 player.GetComponent(FPSInputController).horizontalAxis = 0;
+					  	 cooldown = dpadSecondCooldown;
+ 		 						
+				
+		    }
+		}
+	
+		
+		//player has stopped holding down on directional button.  Character stops.
+			if(touch.phase == TouchPhase.Ended){
+				Debug.Log ("The Touch has ended");
+				player.GetComponent(Movement).MoveStop();
+	
+	         	}
+	   
+		
+		
+
+	}
+}
---
+++ Assets/Scripts/Movement.js
@@ -0,0 +1,73 @@
+﻿#pragma strict
+
+var ySpeedMultiplier : float = 0;
+var xSpeedMultiplier : float = 0;
+var hp : int;
+private var yVelocity : float;
+private var xVelocity : float;
+
+function Start(){
+//if for some reason HP starts out as zero
+	yVelocity = 0;
+	xVelocity = 0;
+	if( hp <= 0){
+		hp = 1;
+	}
+	 if (xSpeedMultiplier ==0){
+	 	xSpeedMultiplier = 10;
+	 }
+	  if (ySpeedMultiplier ==0){
+	 	ySpeedMultiplier = 10;
+	 }
+}
+
+function MoveUp(){
+	yVelocity = ySpeedMultiplier;
+    Debug.Log ("Moving up");
+
+}
+
+function MoveDown(){
+	yVelocity = -ySpeedMultiplier;
+	    Debug.Log ("Moving down");
+}
+
+function MoveStop(){
+	yVelocity = 0;
+
+}
+
+function Update(){
+	
+	    transform.position.y += yVelocity * Time.deltaTime;
+
+}
+
+function FixedUpdate(){
+	if(hp > 0){
+		transform.parent.position.x += xSpeedMultiplier * Time.deltaTime;	}	
+}
+
+function OnCollisionStay(collision : Collision) {
+		// Check if the collider we hit has a rigidbody
+		// Then apply the force
+		if (collision.transform.tag == "Untagged" && hp > 0) {
+			Camera.mainCamera.transform.parent = null;
+			hp = 0;
+				transform.rigidbody.useGravity = true;
+				transform.rigidbody.AddRelativeTorque (100, 200, -100);
+			    transform.rigidbody.AddForce (-1000, 1000, -1000);
+
+			
+			
+		}
+	}
+//
+//function OnTriggerEnter(other: Collider){
+//	
+//	if(other.tag == "Untagged" && hp > 0){
+//	Debug.Log ("Collision");
+//	 hp = 0;
+//	 transform.rigidbody.AddForce (-100, 10, 0);
+//	}
+//}
